3562bd63b97045efd4514b93de8de46ea1f4ccbb
gilang <gilang.mhs13@ti.poligon.ac.id>
2016-03-21 17:16:05 +0700

    Kirim Order

---
+++ app/src/main/java/pusat/android/makananbekuenak/com/aplikasi-distributor/KirimOrder.java
@@ -0,0 +1,89 @@
+package pusat.android.makananbekuenak.com.aplikasi_distributor;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class KirimOrder extends Activity {
+
+    Button btnKirim, btnReset,btnPrs, btnCancel;
+    EditText nmr, nama, market, nml, nomoResi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kirim_order);
+
+        btnKirim = (Button) findViewById(R.id.btnSubmit);
+        btnReset = (Button) findViewById(R.id.btnReset);
+
+        nmr = (EditText) findViewById(R.id.nmOrder);
+        nama = (EditText) findViewById(R.id.nmPenerima);
+        market = (EditText) findViewById(R.id.nmMarket);
+        nml = (EditText) findViewById(R.id.nominal);
+
+        btnKirim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotifikasiResi();
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                nmr.setText("");
+                nama.setText("");
+                market.setText("");
+                nml.setText("");
+
+            }
+        });
+    }
+
+    public void NotifikasiResi() {
+        //
+        final Dialog dialog = new Dialog(KirimOrder.this);
+        dialog.setContentView(R.layout.kirim_order_noresi);
+        dialog.setCancelable(true);
+        dialog.setTitle("Masukan Nomor Resi");
+
+        nomoResi = (EditText)dialog.findViewById(R.id.nmrResi);
+
+        btnPrs = (Button)dialog.findViewById(R.id.btnProses);
+        btnCancel = (Button)dialog.findViewById(R.id.btnKembali);
+        dialog.show();
+
+        btnPrs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tombol Proses Order
+                Toast.makeText(KirimOrder.this, "Berhasil Memasukan Nomor Resi", Toast.LENGTH_LONG).show();
+                dialog.cancel();
+
+                // Pindah Screen
+                Intent i = new Intent(KirimOrder.this, pusat.android.makananbekuenak.com.aplikasi_distributor.MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tombol Batal
+                Toast.makeText(KirimOrder.this, "Anda Tidak Memasukan Nomor Resi", Toast.LENGTH_LONG).show();
+                dialog.cancel();
+                // bersihkan
+                nmr.setText("");
+                nama.setText("");
+                market.setText("");
+                nml.setText("");
+            }
+        });
+    }
+}
