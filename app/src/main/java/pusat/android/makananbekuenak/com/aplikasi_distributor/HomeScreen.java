60ecf617ce98e1673487647882454f772f1ad345
Recanansyah <luffy2460@gmail.com>
2016-03-30 15:47:18 +0700

    login

--- app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_distributor/HomeScreen.java
+++ app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_distributor/HomeScreen.java
@@ -3,6 +3,8 @@ package pusat.android.makananbekuenak.com.aplikasi_distributor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,39 +17,52 @@ public class HomeScreen extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
 
-        Button pengaturan = (Button) findViewById(R.id.pengaturan);
-        Button kirimorder = (Button) findViewById(R.id.pesanan);
-        Button laporan = (Button) findViewById(R.id.laporan);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
 
-        //--Pengaturan
-        pengaturan.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Pengaturan.class);
-                startActivity(intent);
-            }
-        });
-
-        //Pesanan
-        kirimorder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent panggil_class = new Intent(getApplicationContext(), KirimOrder.class);
-                startActivity(panggil_class);
-            }
-        });
-
-        //--Produk
-
-        //Laporan
-
-        laporan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent panggil_class = new Intent(getApplicationContext(), Laporan.class);
-                startActivity(panggil_class);
-            }
-        });
-
-        //Kotak Masuk
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void btn_pengaturan(View v) {
+
+        Intent panggil_class = new Intent(this, pusat.android.makananbekuenak.com.aplikasi_distributor.Pengaturan.class);
+        startActivity(panggil_class);
+    }
+
+    public void btn_kirimorder(View v) {
+
+        Intent panggil_class = new Intent(this, pusat.android.makananbekuenak.com.aplikasi_distributor.KirimOrder.class);
+        startActivity(panggil_class);
+    }
+
+    public void btn_laporan(View v) {
+
+        Intent panggil_class = new Intent(this, pusat.android.makananbekuenak.com.aplikasi_distributor.Laporan.class);
+        startActivity(panggil_class);
+    }
+
+
+
 }
