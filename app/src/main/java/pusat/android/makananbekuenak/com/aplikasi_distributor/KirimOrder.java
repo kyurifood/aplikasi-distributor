d533fee360dec5561c5c6c948abc9b3ea4f3179f
gilang <gilang.mhs13@ti.poligon.ac.id>
2016-04-14 11:12:39 +0700

    initial

--- app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_distributor/KirimOrder.java
+++ app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_distributor/KirimOrder.java
@@ -1,6 +1,5 @@
 package pusat.android.makananbekuenak.com.aplikasi_distributor;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,8 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import pusat.android.makananbekuenak.com.aplikasi_distributor.HomeScreen;
 
 public class KirimOrder extends AppCompatActivity {
 
