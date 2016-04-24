0c672be451e7d32f769180616c1f6c1eefcc9b52
gilang <gilang.mhs13@ti.poligon.ac.id>
2016-04-24 23:46:08 +0700

    hshd

--- app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_distributor/Pengaturan.java
+++ app/src/main/java/pusat/android/makananbekuenak/com/aplikasi_distributor/Pengaturan.java
@@ -2,6 +2,7 @@ package pusat.android.makananbekuenak.com.aplikasi_distributor;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class Pengaturan extends AppCompatActivity{
 
@@ -9,5 +10,7 @@ public class Pengaturan extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_pengaturan);
+
+
     }
 }
